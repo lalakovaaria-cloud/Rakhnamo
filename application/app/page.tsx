d9a8b518486f0484fd239e0242a0aad726beab17
mijgonaazizzoda <mijgonai.azizzoda@alif.tj>
2026-03-14 11:31:15 +0500

    deploy app

---
+++ application/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
+      <h1 className="text-5xl font-bold text-black dark:text-white">HELLO</h1>
+    </main>
+  );
+}
